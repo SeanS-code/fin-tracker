5e8d318605fb931b65f14e50b6de3cad91787b41
seansamu <seansamu.code@gmail.com>
2025-04-19 10:49:33 -0400

    created basic crud

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,7 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-@app.route('/')
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["tracker"]
+collection = db["expense"]
+
+# Base page
+@app.route('/', methods=('GET', 'POST'))
 def root():
-    return "Finance Tracker v0.1"
+    if request.method=='POST':
+        content = request.form['groceries']
+        price = int(content)
+        collection.insert_one({'groceries': price})
+        return redirect(url_for('root'))
+    
+    return render_template('index.html')
+
+# Read
+@app.route('/read', methods=['GET'])
+def read():
+    data = list(collection.find())
+    for d in data:
+        d['_id'] = str(d['_id'])
+    return jsonify(data)
+
+# Update
+@app.route('/users/<id>', methods=['GET','PUT'])
+def update_user(id):
+    data = request.get_json()
+    result = collection.update_one({"_id": ObjectId(id)}, {"$set": data})
+    if result.matched_count:
+        return jsonify({"msg": "Updated successfully"})
+    return jsonify({"error": "User not found"}), 404
+
+# Delete
+@app.route('/del/<id>', methods=['GET','DELETE'])
+def delete(id):
+    result = collection.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count:
+        return jsonify({"msg": "Deleted successfully"})
+    return jsonify({"error": "User not found"}), 404
