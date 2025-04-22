4988bdfc9fea7a4ad08449bb865408caa9fa5d02
seansamu <seansamu.code@gmail.com>
2025-04-22 06:32:34 -0400

    fixed bug with post methods, and added delete functionality

--- flaskr/main.py
+++ flaskr/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, url_for, redirect, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
+from datetime import datetime
+
 app = Flask(__name__)
 
 # Connect to MongoDB
@@ -9,17 +11,29 @@ client = MongoClient('localhost', 27017)
 db = client.tracker
 collections = db.expenses
 
-# Base page
-@app.route('/', methods=('GET', 'POST'))
+# Homepage
+@app.route('/', methods=['GET'])
 def root():
-    if request.method=='POST':
-        print("Form submitted")
-        category = request.form['categories']
-        value = int(request.form['price'])
-        date = request.form['date']
-        
-        collections.insert_one({'category': category, 'value': value, 'date': date})
-        return redirect(url_for('root'))
-
     all_expenses = collections.find()
     return render_template('index.html', collections=all_expenses)
+
+@app.route('/add', methods=['POST'])
+def add_expense():
+    category = request.form['categories']
+    value = int(request.form['price'])
+    date = request.form['date']
+    
+    document = {
+        'category': category,
+        'value' : value,
+        'date' : date
+    }
+
+    collections.insert_one(document)
+    return redirect(url_for('root'))
+
+@app.route('/delete/<id>', methods=['POST'])
+def delete(id):
+    print("received req")
+    collections.delete_one({ '_id' : ObjectId(id)})
+    return redirect(url_for('root'))
