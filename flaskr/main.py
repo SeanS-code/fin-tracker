f378973152516c3e14911bb7063c172544d7c56c
seansamu <seansamu.code@gmail.com>
2025-04-22 11:41:19 -0400

    added filtering for week, month, 3 months and inserted date as proper datetime

--- flaskr/main.py
+++ flaskr/main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, url_for, redirect, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+from dateutil.relativedelta import relativedelta 
 
 app = Flask(__name__)
 
@@ -17,11 +18,12 @@ def root():
     all_expenses = collections.find()
     return render_template('index.html', collections=all_expenses)
 
+# Add an expense
 @app.route('/add', methods=['POST'])
 def add_expense():
     category = request.form['categories']
     value = int(request.form['price'])
-    date = request.form['date']
+    date = datetime.strptime(request.form['date'], '%Y-%m-%d')
     
     document = {
         'category': category,
@@ -32,19 +34,23 @@ def add_expense():
     collections.insert_one(document)
     return redirect(url_for('root'))
 
+# Delete an expense
 @app.route('/delete/<id>', methods=['POST'])
 def delete(id):
     collections.delete_one({ '_id' : ObjectId(id)})
     return redirect(url_for('root'))
 
+# Route user to updating page
 @app.route('/stage_update/<id>', methods=['POST'])
 def stage_update(id):
     return render_template('update.html', _id=id)
+
+# Update given expense
 @app.route('/update/<id>', methods=['POST'])
 def update(id):
     category = request.form['categories']
     value = int(request.form['price'])
-    date = request.form['date']
+    date = datetime.strptime(request.form['date'])
     
     collections.update_one(
         { '_id' : ObjectId(id)}, 
@@ -56,3 +62,30 @@ def update(id):
     )
     print(id)
     return redirect(url_for('root'))
+
+# Filter
+@app.route('/filter', methods=['GET'])
+def filter():
+    filter_value = request.args.get('filter')
+    print(filter_value)
+
+    if filter_value == 'tmonth':
+        # Filter by the past 3 months
+        three_months_ago = datetime.now(timezone.utc) - timedelta(days=90)
+        filtered_expenses = collections.find({"date": {"$gte": three_months_ago}})
+    
+    elif filter_value == 'week':
+        # Filter by the past week
+        one_week_ago = datetime.now(timezone.utc) - timedelta(days=7)
+        filtered_expenses = collections.find({"date": {"$gte": one_week_ago}})
+    
+    elif filter_value == 'month':
+        # Filter by the past month
+        one_month_ago = datetime.now(timezone.utc) - timedelta(days=30)
+        filtered_expenses = collections.find({"date": {"$gte": one_month_ago}})
+    
+    else:
+        # If no valid filter is selected, display all expenses
+        filtered_expenses = collections.find()
+        
+    return render_template('index.html', collections=filtered_expenses)
